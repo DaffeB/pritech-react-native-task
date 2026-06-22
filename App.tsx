ebcf73aecc49ee0c1f2ee1e40853bf60a4467d1c
Dafina Beqiraj <93763212+DaffeB@users.noreply.github.com>
2026-06-22 21:22:18 +0200

    Initial commit: set up project and simplify homescreen

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello Pritech</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#000000',
+    fontSize: 32,
+    fontWeight: '700',
+  },
+});
+
+export default App;
